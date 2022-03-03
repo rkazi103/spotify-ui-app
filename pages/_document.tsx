7c454a252b0a68b829dc4ee2e0740bf6cf8a952d
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-02 19:05:46 -0700

    chore: removed unnecessary things

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
-          <meta name="theme-color" content="#fff" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const Document = () => (
+  <Html lang="en">
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
+      <meta name="theme-color" content="#fff" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
-export default MyDocument;
+export default Document;

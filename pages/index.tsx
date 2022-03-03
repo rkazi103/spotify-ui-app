33344072a8a8581c67735c948d7002bd29e64ba1
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-02 19:11:26 -0700

    feat: partially implemented sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="h-screen overflow-hidden bg-black">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hi</h1>
+      <main>
+        <Sidebar />
+      </main>
     </div>
   );
 };

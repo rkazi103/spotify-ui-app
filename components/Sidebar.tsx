b6d05667df6a05d509b9130d3a5b3ea54aa6962e
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-03-04 19:07:47 -0700

    feat: built login page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,11 +7,23 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
+import { signOut, useSession } from "next-auth/react";
 
 const Sidebar: NextComponentType = () => {
+  const { data: session } = useSession();
+
+  console.log(session);
+
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Logout ❌</p>
+        </button>
+
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
---
+++ pages/auth/login.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import type { GetServerSideProps, NextPage } from "next";
+import type { Provider } from "next-auth/providers";
+import { getProviders, signIn } from "next-auth/react";
+
+type LoginProps = {
+  providers: object;
+};
+
+const Login: NextPage<LoginProps> = ({ providers }) => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <img src="/images/spotify.png" alt="Spotify Logo" className="mb-5 w-52" />
+
+      {providers &&
+        Object.values(providers).map((provider: Provider) => (
+          <div key={provider.name}>
+            <button
+              className="rounded-full bg-[#18D860] p-5 text-white"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Login with {provider.name}
+            </button>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+};
+
+export default Login;
